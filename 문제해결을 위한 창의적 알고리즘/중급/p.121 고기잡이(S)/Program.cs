ebe4f6d0190fb44708eb9861169d77e56a6a5ca1
Kyungbin Cho <diesuki4@gmail.com>
2022-07-12 12:44:21 +0900

    p.121 고기잡이(S) 해결

---
+++ 문제해결을 위한 창의적 알고리즘/중급/p.121 고기잡이(S)/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+
+public class Program
+{
+	public static void Main(string[] args)
+	{
+		int n = int.Parse(Console.ReadLine());
+		int netSize = int.Parse(Console.ReadLine());
+		int[] fishes = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+
+		Console.WriteLine(fishes.Where((x, i) => i <= n-netSize).Select((x, i) => fishes[i] + fishes[i+1] + fishes[i+2]).Max());
+	}
+}
