1810e3c60da46386255f09627626cb1c6b70db2a
Kyungbin Cho <diesuki4@gmail.com>
2022-07-12 12:51:14 +0900

    p.42 최댓값(S) 해결

---
+++ 문제해결을 위한 창의적 알고리즘/중급/p.42 최댓값(S)/diesuki4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+public class Program
+{
+	public static void Main(string[] args)
+	{
+		int max = 0, index = 1;
+
+		for (int i = 1; i <= 9; i++)
+		{
+			int n = int.Parse(Console.ReadLine());
+
+			if (max < n)
+			{
+				max = n;
+				index = i;
+			}
+		}
+
+		Console.WriteLine(max);
+		Console.WriteLine(index);
+	}
+}
