c22ce1f8e2207795f3f6094cd7a596cc87a25a28
Kyungbin Cho <diesuki4@gmail.com>
2022-07-12 12:28:24 +0900

    p.77 두더지 굴(S) 해결

---
+++ 문제해결을 위한 창의적 알고리즘/중급/p.77 두더지 굴(S)/diesuki4/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+
+public class Program
+{
+	public static void Main(string[] args)
+	{
+		int n = int.Parse(Console.ReadLine());
+		int[,] arr = new int[n, n];
+		List<int> sizes = new List<int>();
+
+		for (int i = 0; i < n; ++i)
+		{
+			for (int j = 0; j < n; ++j)
+			{
+				arr[i, j] = Console.Read() - '0';
+				Console.Read();
+			}	
+			Console.Read();
+		}
+
+		for (int i = 0; i < n; ++i)
+			for (int j = 0; j < n; ++j)
+				if (arr[i, j] == 1)
+					sizes.Add(OysterSize(arr, i, j));
+
+		Console.WriteLine(sizes.Count());
+
+		foreach (int size in sizes.OrderByDescending(x => x))
+			Console.WriteLine(size);
+	}
+
+	public static int OysterSize(int[,] arr, int i, int j)
+	{
+		int n = arr.GetLength(0);
+
+		if (i < 0 || n <= i || j < 0 || n <= j || arr[i, j] == 0)
+			return 0;
+		else
+			arr[i, j] = 0;
+
+		return 1 + OysterSize(arr, i-1, j) + OysterSize(arr, i+1, j) + OysterSize(arr, i, j-1) + OysterSize(arr, i, j+1);
+	}
+}
