d8bd346353780d822486f54d76bf39956a608575
Kyungbin Cho <diesuki4@gmail.com>
2022-08-05 21:56:29 +0900

    2979. 트럭 주차 해결

---
+++ 백준/2979. 트럭 주차/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+	int price[4] = {0};
+	int trucksInMinute[101] = {0};
+	int totalPrice = 0;
+
+	cin >> price[1] >> price[2] >> price[3];
+
+	for (int i = 0; i < 3; ++i)
+	{
+		int from, to;
+
+		cin >> from >> to;
+
+		for (int j = ++from; j <= to; ++j)
+			++trucksInMinute[j];
+	}
+
+	for (int truck : trucksInMinute)
+		totalPrice += truck * price[truck];
+
+	cout << totalPrice;
+}
