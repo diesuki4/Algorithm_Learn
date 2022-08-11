c4bdea8ba2a206270e5211e2204b1e7f177876d6
Kyungbin Cho <diesuki4@gmail.com>
2022-08-11 20:38:10 +0900

    주석 추가
    1159. 농구 경기
    11655. ROT13

--- 백준/1159. 농구 경기/main.cpp
+++ 백준/1159. 농구 경기/main.cpp
@@ -8,6 +8,7 @@ int main(int argc, char* argv[])
 	int numAlpha[26] = {0};
 	bool isPredaja = true;
 
+	// 이름의 개수
 	cin >> n;
 
 	for (int i = 0; i < n; ++i)
@@ -16,16 +17,23 @@ int main(int argc, char* argv[])
 
 		cin >> s;
 
+		// 0(a) ~ 25(z) 를 나타내는 배열의
+		// 해당 위치에 +1
 		++numAlpha[s[0] - 'a'];
 	}
 
+	// a 부터 z 까지
 	for (int i = 0; i < 26; ++i) {
+		// 해당 알파벳으로 시작하는 성이 5명 이상이면
 		if (5 <= numAlpha[i]) {
+			// 알파벳을 출력한다.
 			cout << static_cast<char>('a' + i);
+			// 선수 선발이 불가능하지 않다
 			isPredaja = false;
 		}
 	}
 
+	// 선수 선발이 불가능하면 출력
 	if (isPredaja)
 		cout << "PREDAJA";
 
--- 백준/11655. ROT13/main.cpp
+++ 백준/11655. ROT13/main.cpp
@@ -3,14 +3,19 @@
 
 using namespace std;
 
+// (문자열 >> 정수) 형식의 연산을 오버로딩 한다.
+// string ROT13(string s, int n) 과 동일
 string operator >> (string s, int n)
 {
 	int length = s.length();
 
 	for (int i = 0; i < length; ++i)
 	{
+		// char (-128 ~ 127) 범위를 넘어가는 문자가 있으므로 unsigned
 		unsigned char uch = s[i] + n;
 
+		// 더해서 'z' 혹은 'Z' 를 넘어가면 26을 빼주고
+		// 넘어가지 않으면 그냥 대입한다.
 		if ('N' <= uch)
 			 s[i] = (uch <= 'Z') || (('n' <= uch) && (uch <= 'z')) ?
 						uch : uch - 26;
@@ -25,6 +30,7 @@ int main(int argc, char* argv[])
 
 	getline(cin, s);
 
+	// ROT13 수행 후 출력
 	cout << (s >> 13);
 
 	return 0;
