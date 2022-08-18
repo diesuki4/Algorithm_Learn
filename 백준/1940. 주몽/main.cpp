2aeaf3cf7ba02b4eb34bdc613acf3c36da3788db
Kyungbin Cho <diesuki4@gmail.com>
2022-08-19 05:52:01 +0900

    1940. 주몽 주석 작성

--- 백준/1940. 주몽/main.cpp
+++ 백준/1940. 주몽/main.cpp
@@ -15,19 +15,25 @@ int main(int argc, char* argv[])
 
 	cin >> N >> M;
 
+	// 재료의 개수만큼 반복
 	while (N--)
 	{
 		int num;
 
+		// 현재 고유 번호 입력
 		cin >> num;
 
+		// 벡터를 순회
 		for (int elem : vec) {
+			// 벡터의 원소 + 현재 고유 번호 == 정답 고유 번호의 합
 			if (elem + num == M) {
+				// 정답 1 증가
 				++answer;
 				break;
 			}
 		}
 		
+		// 벡터에 고유 번호를 삽입
 		vec.push_back(num);
 	}
 
