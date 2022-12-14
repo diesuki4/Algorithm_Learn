e0c87ebe488254937398bf1c1f4d08e047f0a365
Kyungbin Cho <diesuki4@gmail.com>
2022-12-14 13:35:58 +0900

    Level 0. 두 수의 합 해결

---
+++ 프로그래머스/Level 0/두 수의 합/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int solution(int num1, int num2);
+
+int main(int argc, char* argv[])
+{
+	cout << solution(2, 3) << endl;
+	cout << solution(100, 2) << endl;
+
+	return 0;
+}
+
+int solution(int num1, int num2)
+{
+    return num1 + num2;
+}
