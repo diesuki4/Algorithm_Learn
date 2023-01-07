94d60207956ab2c1208d673f93717fd9b134a5f7
Kyungbin Cho <diesuki4@gmail.com>
2023-01-07 11:24:54 +0900

    Level 0. 팩토리얼 해결

---
+++ 프로그래머스/Level 0/팩토리얼/README.md
@@ -0,0 +1,4 @@
+# Algorithm_Learn
+### 2023-01-07 (토)
+[팩토리얼](https://school.programmers.co.kr/learn/courses/30/lessons/120848)
+- 구현
---
+++ 프로그래머스/Level 0/팩토리얼/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int solution(int n);
+
+int main()
+{
+    cout << solution(3628800) << endl;
+    cout << solution(7) << endl;
+}
+
+int solution(int n)
+{
+    const int arr[] = {1, 2, 6, 24, 120, 720, 5040, 40320, 362880, 3628800};
+    size_t size = sizeof(arr) / sizeof(*arr);
+
+    return upper_bound(arr, arr + size, n) - arr;
+}
