8f2a90f3b5226baf26f4e825676925638cbebd08
Kyungbin Cho <diesuki4@gmail.com>
2023-01-11 09:02:00 +0900

    Level 0. 로그인 성공? 해결

---
+++ 프로그래머스/Level 0/로그인 성공？/README.md
@@ -0,0 +1,4 @@
+# Algorithm_Learn
+### 2023-01-11 (수)
+[로그인 성공?](https://school.programmers.co.kr/learn/courses/30/lessons/120883)
+- 구현
---
+++ 프로그래머스/Level 0/로그인 성공？/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+string solution(vector<string> id_pw, vector<vector<string>> db);
+
+int main()
+{
+    cout << solution({"meosseugi", "1234"}, {{"rardss", "123"}, {"yyoom", "1234"}, {"meosseugi", "1234"}}) << endl;
+    cout << solution({"programmer01", "15789"}, {{"programmer02", "111111"}, {"programmer00", "134"}, {"programmer01", "1145"}}) << endl;
+    cout << solution({"rabbit04", "98761"}, {{"jaja11", "98761"}, {"krong0313", "29440"}, {"rabbit00", "111333"}}) << endl;
+}
+
+string solution(vector<string> id_pw, vector<vector<string>> db)
+{
+    for (vector<string> row : db)
+        if (row[0] == id_pw[0])
+            return (row[1] == id_pw[1]) ? "login" : "wrong pw";
+
+    return "fail";
+}
