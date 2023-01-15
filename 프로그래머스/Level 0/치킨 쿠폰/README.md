3b97c80b8a914c2f4f7f3b088d3e57aa31883863
Kyungbin Cho <diesuki4@gmail.com>
2023-01-15 09:21:54 +0900

    Level 0. 치킨 쿠폰 해결

---
+++ 프로그래머스/Level 0/치킨 쿠폰/README.md
@@ -0,0 +1,4 @@
+# Algorithm_Learn
+### 2023-01-15 (일)
+[치킨 쿠폰](https://school.programmers.co.kr/learn/courses/30/lessons/120884)
+- 구현
---
+++ 프로그래머스/Level 0/치킨 쿠폰/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int solution(int chicken);
+
+int main()
+{
+    cout << solution(100) << endl;
+	cout << solution(1081) << endl;
+}
+
+int solution(int chicken)
+{
+	int answer = 0;
+
+	do
+	{
+		int t = chicken % 10;
+
+		answer += (chicken /= 10);
+
+		chicken += t;
+	}
+	while (10 <= chicken);
+
+	return answer;
+}
