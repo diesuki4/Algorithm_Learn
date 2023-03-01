850139fed9866f40ede0600a2bd4572530d4c891
Kyungbin Cho <diesuki4@gmail.com>
2023-03-01 09:17:37 +0900

    10172. 개 해결

---
+++ 백준/10172. 개/README.md
@@ -0,0 +1,4 @@
+# Algorithm_Learn
+### 2023-03-01 (수)
+[10172. 개](https://www.acmicpc.net/problem/10172)
+- 문자열
---
+++ 백준/10172. 개/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+    string s =  "|\\_/|\n"
+                "|q p|   /}\n"
+                "( 0 )\"\"\"\\\n"
+                "|\"^\"`    |\n"
+                "||_/=\\\\__|";
+    
+    cout << s;
+    
+    return 0;
+}
