590925a1528c5da4f047cdbe9891e2eb3ee5698e
Kyungbin Cho <diesuki4@gmail.com>
2023-04-06 22:35:59 +0900

    Level 2. 스킬트리 해결

---
+++ 프로그래머스/Level 2/스킬트리/main.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <numeric>
+
+using namespace std;
+
+int solution(string skill, vector<string> skill_trees);
+
+int main(int argc, char* argv[])
+{
+    cout << solution("CBD", {"BACDE", "CBADF", "AECB", "BDA"}) << endl;
+
+    return 0;
+}
+
+int solution(string skill, vector<string> skill_trees)
+{
+    int answer = 0;
+    map<char, int> mp;
+
+    for (int i = 0; i < skill.length(); ++i)
+        mp[skill[i]] = i + 1;
+
+    for (string& sk_tree : skill_trees)
+    {
+        char last = '\0';
+        bool bSuccess = true;
+
+        for (char c : sk_tree)
+        {
+            if (mp[c] - 1 == mp[last])
+            {
+                if (c == prev(mp.end())->first)
+                    break;
+                else
+                    last = c;
+            }
+            else if (mp[c])
+            {
+                bSuccess = false;
+                break;
+            }
+        }
+
+        if (bSuccess)
+            ++answer;
+    }
+
+    return answer;
+}
