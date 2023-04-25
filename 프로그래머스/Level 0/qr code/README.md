85fac8976b81f638d84a4fc4e81f2eb21f12e6f7
Kyungbin Cho <diesuki4@gmail.com>
2023-04-25 16:12:10 +0900

    Level 0. qr code 해결

---
+++ 프로그래머스/Level 0/qr code/README.md
@@ -0,0 +1,4 @@
+# Algorithm_Learn
+### 2023-04-25 (화)
+[qr code](https://school.programmers.co.kr/learn/courses/30/lessons/181903)
+- 구현
---
+++ 프로그래머스/Level 0/qr code/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+string solution(int q, int r, string code);
+
+int main(int argc, char* argv[])
+{
+    cout << solution(3, 1, "qjnwezgrpirldywt") << endl;
+    cout << solution(1, 0, "programmers") << endl;
+
+    return 0;
+}
+
+string solution(int q, int r, string code)
+{
+    string answer;
+
+    for (int i = 0; i < code.length(); ++i)
+        if (i % q == r)
+            answer.push_back(code[i]);
+
+    return answer;
+}
