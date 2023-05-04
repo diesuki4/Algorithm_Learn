f0a62741350f8c77bbe9bd20fbe15425d709abd0
Kyungbin Cho <diesuki4@gmail.com>
2023-05-04 09:26:50 +0900

    Level 0. 전국 대회 선발 고사 해결

---
+++ 프로그래머스/Level 0/전국 대회 선발 고사/README.md
@@ -0,0 +1,4 @@
+# Algorithm_Learn
+### 2023-05-04 (목)
+[전국 대회 선발 고사](https://school.programmers.co.kr/learn/courses/30/lessons/181851)
+- 구현
---
+++ 프로그래머스/Level 0/전국 대회 선발 고사/main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+#include <map>
+
+using namespace std;
+
+int solution(vector<int> rank, vector<bool> attendance);
+
+int main(int argc, char* argv[])
+{
+    cout << solution({3, 7, 2, 5, 4, 6, 1}, {false, true, true, true, true, false, false}) << endl;
+    cout << solution({1, 2, 3}, {true, true, true}) << endl;
+    cout << solution({6, 1, 5, 2, 3, 4}, {true, false, true, false, false, true}) << endl;
+
+    return 0;
+}
+
+int solution(vector<int> rank, vector<bool> attendance)
+{
+    int answer = 0, cur = 0;
+    int arr[] = {10'000, 100, 1};
+    size_t size = rank.size();
+    map<int, int> mp;
+
+    for (int i = 0; i < size; ++i)
+        mp[rank[i]] = attendance[i] ? i : -1;
+
+    for (int i = 1; i <= size; ++i)
+    {
+        if (mp[i] != -1)
+        {
+            answer += arr[cur++] * mp[i];
+
+            if (cur == 3)
+                break;
+        }
+    }
+
+    return answer;
+}
