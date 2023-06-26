cccb95837d12fa75e6aab2c7ad108b83769c9ef6
Kyungbin Cho <diesuki4@gmail.com>
2023-06-26 10:46:49 +0900

    Level 2. 다리를 지나는 트럭 해결

---
+++ 프로그래머스/Level 2/다리를 지나는 트럭/README.md
@@ -0,0 +1,5 @@
+# Algorithm_Learn
+### 2023-06-26 (월)
+[다리를 지나는 트럭](https://school.programmers.co.kr/learn/courses/30/lessons/42583)
+- 큐
+- 시뮬레이션
---
+++ 프로그래머스/Level 2/다리를 지나는 트럭/main.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <vector>
+#include <deque>
+
+using namespace std;
+
+int solution(int bridge_length, int weight, vector<int> truck_weights);
+
+int main(int argc, char* argv[])
+{
+    cout << solution(2, 10, {7, 4, 5, 6}) << endl;
+    cout << solution(100, 100, {10}) << endl;
+    cout << solution(100, 100, {10, 10, 10, 10, 10, 10, 10, 10, 10, 10}) << endl;
+
+    return 0;
+}
+
+int solution(int bridge_length, int weight, vector<int> truck_weights)
+{
+    int answer = 0;
+    int nTrucks = truck_weights.size(), nPassed = 0, nWeight = 0;
+    deque<int> trucks(truck_weights.begin(), truck_weights.end()), bridge(bridge_length, 0);
+
+    while (nPassed != nTrucks)
+    {
+        if (int elem = bridge.front())
+            ++nPassed, nWeight -= elem;
+
+        bridge.pop_front();
+
+        if (trucks.empty())
+        {
+            ;
+        }
+        else if (nWeight + trucks.front() <= weight)
+        {
+            nWeight += trucks.front();
+            bridge.emplace_back(trucks.front());
+            trucks.pop_front();
+        }
+        else
+        {
+            bridge.emplace_back(-1);
+        }
+
+        ++answer;
+    }
+
+    return answer;
+}
